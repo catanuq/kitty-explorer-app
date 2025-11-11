b0beff1f9ec06cd6d479eed9b93344f5a4b2351f
catanuq <catanuq2005@gmail.com>
2025-11-11 17:29:30 +0200

    init comit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Frame frame = new Frame();
